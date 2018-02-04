c42e38c7868af8f6f8cbe57613cce469f79f709f
Philipp Dargel <chisui@informatik.uni-bremen.de>
2018-02-04 22:29:12 +0000

    fixed shim

--- scripts/buildShellShim.zsh
+++ scripts/buildShellShim.zsh
@@ -2,9 +2,8 @@ if [ $1 = "--rcfile" ]; then
   # the rcfile flag indicates that the --command option was used.
   # This means the shell should stay open after executing. So we remove the last line which contains 'exit'
   shift
-  tmp="$(dirname $1)/tmp"
-  sed '$d' $1 > $tmp
-  mv $tmp $1
+  tmp="$(cat $1)"
+  echo ${tmp%$exit} > $1
 fi
 echo -e "\nzsh" >> $1
 bash $1
