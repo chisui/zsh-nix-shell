f057b105a228c7c3e821aa1daf2f58b1df7da1da
Philipp Dargel <chisui@informatik.uni-bremen.de>
2018-02-04 21:54:06 +0000

    reworked rcfile handling

--- scripts/buildShellShim.zsh
+++ scripts/buildShellShim.zsh
@@ -1,9 +1,10 @@
 if [ $1 = "--rcfile" ]; then
-  # as last step of rc file start zsh
-  echo -e "\nzsh" >> $2
-  bash $2
-else
-  # if then command should simply be executed lets use bash
-  # since we only need zsh for interactive stuff
-  bash $1
+  # the rcfile flag indicates that the --command option was used.
+  # This means the shell should stay open after executing. So we remove the last line which contains 'exit'
+  shift
+  tmp="$(dirname $1)/tmp"
+  sed '$d' $1 > $tmp
+  mv $tmp $1
 fi
+echo -e "\nzsh" >> $1
+bash $1
