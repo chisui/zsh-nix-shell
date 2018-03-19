81fb4ea207987055497685645ca86f04d65469d8
Philipp Dargel <chisui@informatik.uni-bremen.de>
2018-03-19 09:57:34 +0000

    use shell features instead of sed + indentation

--- nix-shell.plugin.zsh
+++ nix-shell.plugin.zsh
@@ -3,8 +3,8 @@ NIX_SHELL_PLUGIN_DIR=${0:a:h}
 
 # extracts packages argument from args and passes them in $NIX_SHELL_PACKAGES variable.
 function nix-shell() {
-  local REAL_NIX_SHELL=$(whereis -b nix-shell | sed -e "s/^nix-shell: //")
-  local ARGS=( "$@" )
+  local REAL_NIX_SHELL=${"$(whereis -b nix-shell)"#nix-shell:\ }
+  local ARGS=("$@")
   local NIX_SHELL_PACKAGES=""
 
   # extract -p|--packages argument into NIX_SHELL_PACKAGES
@@ -13,24 +13,28 @@ function nix-shell() {
   do
     key=${ARGS[1]}
     # enter "--packages packages..." mode
-    if [[ $key = "-p" || $key = "--packages" ]]; then
+    if [[ $key = "-p" || $key = "--packages" ]]
+    then
       IN_PACKAGES=1
       NIX_SHELL_PACKAGES+=${ARGS[2]}
       ARGS=("${ARGS[@]:1}")
 
     # skip "--arg name value" argument
-    elif [[ $key = "--arg" ]]; then
+    elif [[ $key = "--arg" ]]
+    then
       IN_PACKAGES=0
       ARGS=("${ARGS[@]:2}")
 
     # skip all other unary arguments 
-    elif [[ $key == "-"* ]]; then
+    elif [[ $key == "-"* ]]
+    then
       IN_PACKAGES=0
       ARGS=("${ARGS[@]:1}")
 
     # If we don't have any argument prefix we are either in package mode
     # or we have encountered the path argument
-    elif [[ $IN_PACKAGES = 1 ]]; then
+    elif [[ $IN_PACKAGES = 1 ]]
+    then
       NIX_SHELL_PACKAGES+=" $key"
     fi
     ARGS=("${ARGS[@]:1}")
