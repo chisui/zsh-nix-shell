0a8c689304b376bc353aaf4078bc3ef74378a8a3
Philipp Dargel <chisui@informatik.uni-bremen.de>
2018-06-29 19:12:07 +0200

    removed debug output

--- scripts/buildShellShim.zsh
+++ scripts/buildShellShim.zsh
@@ -2,7 +2,6 @@ if [ $1 = "--rcfile" ]; then
   # the rcfile flag indicates that the --command option was used.
   # This means the shell should stay open after executing. So we remove the last line which contains 'exit'
   shift
-  echo "rcfile"
   tmp="$(cat $1)"
   echo ${tmp%exit} > $1
   echo "zsh" >> $1
